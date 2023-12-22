d63a31e87d299f5bea6d233d4b63aee9827f8f6c
amubacr <amubacraft1201@gmail.com>
2023-12-22 18:18:00 +0800

    Refactor: * Refactor HTML, CSS, and JS Code in development Branch
    * Changing file and folder structure in this project repository
    
    Feat [Header]: + Adding Header and Navigation
    
    Style [Header]: + Styling Header and Navigation in normal view and all responsive view

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function openbtn() {
-    document.getElementById("mobile-menu").style.display = "flex";
-}
-
-function collapsebtn() {
-    document.getElementById("mobile-menu").style.display = "none";
-}
